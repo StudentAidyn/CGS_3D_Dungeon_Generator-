8ddbb3cb33ae681abf089f68f0a078139d4160ad
StudentAidyn <AIES215175@gmail.com>
2024-05-01 19:55:29 +1000

    Updated, added sudo code plan

--- CGS_Dungeon_Generator_plugin/Assets/Scripts/Sc_.cs
+++ CGS_Dungeon_Generator_plugin/Assets/Scripts/Sc_.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Sc_ : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
+/*
+1). algorithm that can assign what each tile can be connected to just by using their own sides.
+this could work by assigning names to each side that would correlate with how they should interact.
+
+for each side correlating rules apply:
+
+for each side a numbers will be used to distinguish the difference between them.
+
+if an 'S' follows the number that states the sides are symetrical,
+
+if an 'F' follows the number that states it is a flip of number previously mentioned.
+
+for vertical sides a combination is used to declare that it is a Vertical side, the top or bottom of the module,the section type, and the current rotation.
+for example the Combination V1_30 declares it is a vertical section on the TOP with 3 sections at the rotation 0.
+This would mean that the only connector is a V0_30
+
+
+2). Collapsing - Every Square, Any thing all at once
+All Squares at the start can be anything, create a container for each possible module that contains all possabilities. 
+
+When a square is collapsed at the start of the sequence:
+i).     the initial surrounding spaces (all 6) will have all non-possible meshes removed from its possabilities container
+ii).    then with each collapsed square the possabilities of each connector piece are checked by looping through and comparing the possible pieces within each module against neighbouring module, this repeats until all modules have their possablitlies checked
+
+BUT
+
+ii).    THE REDO: if instead of forcing the calculation to check all squares everytime the area will be blocked out and only generated within that space, this will be done by creating a clamp.
+iii).   Then the code will start the full collapse pattern:
+    a).     checking for the lowest entropy module then collapsing it, 
+    b).     applying the changes to the surrounding modules based on the collapsed module
+    c).     repeat, until all modules are collapsed.
+
+
+
+ 
+ */
--- CGS_Dungeon_Generator_plugin/Assets/Scripts/Sc_MapGenerator.cs
+++ CGS_Dungeon_Generator_plugin/Assets/Scripts/Sc_MapGenerator.cs
@@ -8,9 +8,10 @@ class Sc_MapGenerator : MonoBehaviour
     // size x and z will be horizontal and Y will be vertical, basically resembling the total number of floors.
     [SerializeField] const int SIZE_X = 1, SIZE_Y = 1, SIZE_Z = 1;
     // the Wave Function Collapse 3D Array Container
-    WFCObject[,] m_WFC = new WFCObject[SIZE_X, SIZE_Y];
+    WFCModule[,] m_WFC = new WFCModule[SIZE_X, SIZE_Y];
     
-
+    /// this WFC cycles through the whole array every time,
+    /// using overlapping chunks will help with performance and accuracy.
 
     private void Start()
     {
@@ -34,38 +35,90 @@ class Sc_MapGenerator : MonoBehaviour
         {
             for(int j = 0; j < SIZE_Y; j++)
             {
-                if (!m_WFC[i, j].Collapsed()) return false; 
+                if (!m_WFC[i, j].isCollapsed()) return false; 
             }
         }
         return true;
     }
 
-    //private void Iterate()
-    //{
-    //    var coords = GetMinEntropyCoords();
-    //    CollapseAt(coords);
+    private void Iterate()
+    {
+        var coords = GetMinEntropyCoords();
+        CollapseAt(coords);
+        Propagate(coords);
 
+    }
 
-    //}
+    private void Propagate(Vector2 coords)
+    {
 
-    //Vector2 GetMinEntropyCoords()
-    //{
-    //    List<Vector2> temp = new List<Vector2>();
-    //    for (int i = 0; i < SIZE_X; i++)
-    //    {
-    //        for (int j = 0; j < SIZE_Y; j++)
-    //        {
-    //            temp.Add()
-    //        }
-    //    }
-    //}
+    }
+    // finds and returns the location of *minimum entropy
+    // *if more than 1 it will randomize between modules
+    Vector2 GetMinEntropyCoords()
+    {
+        // cycle through the tiles
+        // find the lowest number (lowest entropy)
+        // cycle through again and check for the number found (checking for doubles)
+        int _lowestEntropy = 1;
+        bool _firstPass = true;
+        for (int i = 0; i < SIZE_X; i++) {
+            for (int j = 0; j < SIZE_Y; j++) {
+                if (!m_WFC[i, j].isCollapsed()) // checks for collapsed modules
+                {
+                    if (_firstPass)
+                    {
+                        _lowestEntropy = m_WFC[i, j].GetEntropy();
+                        _firstPass = false;
+                    }
+                    if (m_WFC[i, j].GetEntropy() < _lowestEntropy)
+                    {
+                        _lowestEntropy = m_WFC[i, j].GetEntropy();
+                    }
+                }
+
+            }
+        }
+
+        // gets all modules that have the same entropy
+        List<Vector2> temp = new List<Vector2>();
+        for (int i = 0; i < SIZE_X; i++)
+        {
+            for (int j = 0; j < SIZE_Y; j++)
+            {
+                if (m_WFC[i, j].GetEntropy() == _lowestEntropy)
+                {
+                    temp.Add(new Vector2(i, j));
+                }
+            }
+        }
+
+        // choosing on random if needed the returned module
+        Vector2 value = new Vector2(0, 0);
+        if(temp.Count > 1) {
+            // if there is more than one, select one at random
+            value = temp[Random.Range(0, temp.Count)];
+        }
+        else
+        {
+            value = temp[0];
+        }
+
+        return value;
+
+    }
+
+    void CollapseAt(Vector2 value) // collapses at the position
+    {
+        m_WFC[(int)value.x, (int)value.y].Collapse();
+    }
     public void CheckRotations()
     {
         // checks each horizontal side
     }
 }
 
-class WFCObject : MonoBehaviour
+class WFCModule : MonoBehaviour
 {
     Mesh _mesh;
     int _currentRotation;
@@ -73,14 +126,19 @@ class WFCObject : MonoBehaviour
     Vector2 _coords;
     
 
-    public bool Collapsed()
+    public bool isCollapsed()
     {
         return _collapsed;
     }
 
-    public void Collaps()
+    public void Collapse()
     {
         // collapses the current tile based on the principle that it has the lowest entropy so it must close, if there is more than one ooption apply randomization
+    }
+
+    public int GetEntropy()
+    {
+        return 1;
     }
 
 }
